c77806a9a438603f0d3393549af5837c5986449f
KK-Sharma-Lab <86070499+KK-Sharma-Lab@users.noreply.github.com>
2021-06-19 20:10:05 +0530

    Update metalloenzyme_match_single_file.py

--- enzyme_extraction/metalloenzyme_match_single_file.py
+++ enzyme_extraction/metalloenzyme_match_single_file.py
@@ -4,7 +4,7 @@ import numpy as np
 
 import re
 
-enz='/home/pratik/Downloads/metals_cofactors_expasy.xlsx'; #path to file having all metalloenzymes from database
+enz='/path/to/metalloenzymes_list_expasy.xlsx'; #path to file having all metalloenzymes from database
 
 eclist = pd.read_excel(enz) #pandas command to read excel file of metalloenzymes
 
@@ -15,8 +15,8 @@ ec1=eclist.EC_num #assigning EC number column from metalloenzyme file to ec1 var
 ec2=eclist.Enzyme_name #assigning Enzyme names column to ec2 variable
 
 
-samtsv= f"/home/pratik/Downloads/HMP2_healthy/{x}_humann2/{x}_ecs.tsv" # in here the 'f' in the begining of the string allows the integration og variable value in string using '{}', 
-output= f"/home/pratik/Downloads/Metalloenzymes/{x}_me.xlsx" #so the input and output file address will change for each file iterations
+samtsv= f"/path/to/HMP2_healthy/{x}_humann2/{x}_ecs.tsv" # in here the 'f' in the begining of the string allows the integration og variable value in string using '{}', 
+output= f"/path/to/Metalloenzymes/{x}_me.xlsx" #so the input and output file address will change for each file iterations
     
 sample = pd.read_csv(samtsv,delimiter='\t',encoding='utf-8') #code specifies, file is tab delimited by '\t' and encoding  
 
